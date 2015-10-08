c415dae5e978cd42d4ccb8dfba05a83e75fdfdcc
Kent Fredric <kentfredric@gmail.com>
2015-10-08 19:31:53 +1300

    Add beforeuselessblock test and Fix find_first usage it exposed.
    Turns out, find_first doesn't return undef when no results are found,
          but instead returns a false value.
    
    Undef is only for "serious errors" not just 404.

--- lib/PPIx/Element/Sub.pm
+++ lib/PPIx/Element/Sub.pm
@@ -54,10 +54,9 @@ sub identify_next_sub {
         # return the first sub found
         return $sibling if $sibling->isa('PPI::Statement::Sub');
 
-        next unless $sibling->can('find_first');
-
         # If a sibling has children, a sub could be one of them
-        if ( defined( my $result = $sibling->find_first('PPI::Statement::Sub') ) ) {
+        # note: a false value == fail, but explit undef is invalid query
+        if ( $sibling->can('find_first') and my $result = $sibling->find_first('PPI::Statement::Sub') ) {
             return $result;
         }
     }
---
+++ t/corpus/beforeuselessblock.pm
@@ -0,0 +1,9 @@
+# this comment is without a sub following it but requiring sibling traversal
+
+{
+    {
+        {
+
+        }
+    }
+}
